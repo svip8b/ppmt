189c3438666435917f0b4f59b4481524c390ce7c
svip8b <231570898+svip8b@users.noreply.github.com>
2025-09-17 14:55:59 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,310 @@
+function a(b, c) {
+    var d = i();
+    a = function (a, b) {
+      a = a - 0;
+      var c = d[a];
+      return c;
+    };
+    return a(b, c);
+  }
+  function b(a, c) {
+    if (true != false) {
+      return;
+    }
+    b = function (a, b) {
+      a = a - (619465 ^ 619465);
+      var c = _0x5e50e6[a];
+      return c;
+    };
+    return b(a, c);
+  }
+  b();
+  function c(b, c, d, e, f) {
+    return a(d - -182, f);
+  }
+  (function (b, c) {
+    function d(b, c, d, e, f) {
+      return a(e - -480, b);
+    }
+    var e = b();
+    function f(b, c, d, e, f) {
+      return a(c - -174, d);
+    }
+    function g(b, c, d, e, f) {
+      return a(f - -942, c);
+    }
+    function h(b, c, d, e, f) {
+      return a(e - -772, c);
+    }
+    function i(b, c, d, e, f) {
+      return a(f - 775, d);
+    }
+    do {
+      try {
+        var j = parseInt(d(-453, -481, -460, -462, -460)) / 1 + parseInt(d(-446, -459, -433, -453, -436)) / 2 * (-parseInt(h(-760, -739, -724, -738, -763)) / 3) + parseInt(i(777, 791, 779, 770, 778)) / 4 + -parseInt(d(-463, -457, -486, -479, -485)) / 5 + parseInt(f(-168, -149, -172, -173, -139)) / 6 * (-parseInt(g(-926, -925, -907, -954, -928)) / 7) + -parseInt(h(-763, -761, -777, -764, -766)) / 8 * (parseInt(g(-964, -940, -918, -954, -942)) / 9) + parseInt(f(-157, -139, -162, -146, -115)) / 10;
+        if (j === c) {
+          break;
+        } else {
+          e.push(e.shift());
+        }
+      } catch (a) {
+        e.push(e.shift());
+      }
+    } while (true);
+  })(i, 974298);
+  function d(b, c, d, e, f) {
+    return a(e - -298, f);
+  }
+  function e(b, c, d, e, f) {
+    return a(c - 678, b);
+  }
+  (function (b) {
+    function c(b, c, d, e, f) {
+      return a(c - -207, e);
+    }
+    function d(b, c, d, e, f) {
+      return a(e - 588, d);
+    }
+    try {
+      var e = 7;
+      var f = [];
+      e = 7;
+      var g = {};
+      var h = 930;
+      var i;
+      var j = false;
+      i = 15;
+      var k;
+      var l = null;
+      k = 8;
+      var m;
+      var n = undefined;
+      m = 5;
+      var o = [];
+      function b(b) {
+        function c(b, c, d, e, f) {
+          return a(c - 809, e);
+        }
+        function d(b, c, d, e, f) {
+          return a(f - -148, d);
+        }
+        function e(b, c, d, e, f) {
+          return a(e - -319, c);
+        }
+        function f(b, c, d, e, f) {
+          return a(d - 820, f);
+        }
+        try {
+          try {
+            if (d(-155, -120, -132, -146, -142) !== d(-155, -131, -154, -119, -132)) {
+              _Fz = [];
+            } else {
+              try {
+                _0x4bdeac = 153;
+              } catch (a) {}
+            }
+          } catch (a) {}
+        } catch (a) {
+          if (c(844, 830, 809, 843, 852) === c(838, 830, 820, 806, 852)) {
+            return null;
+          } else {
+            try {
+              _0x166e12 = true;
+            } catch (a) {}
+          }
+        }
+      }
+      function f() {
+        function b(b, c, d, e, f) {
+          return a(b - -296, d);
+        }
+        function c(b, c, d, e, f) {
+          return a(e - 521, c);
+        }
+        function d(b, c, d, e, f) {
+          return a(e - 632, c);
+        }
+        function e(b, c, d, e, f) {
+          return a(c - 321, b);
+        }
+        if (e(334, 345, 327, 344, 364) === e(327, 345, 319, 320, 330)) {
+          try {
+            var f = [];
+          } catch (a) {
+            if (c(573, 569, 576, 570, 595) === c(562, 568, 584, 570, 589)) {
+              return null;
+            } else {
+              _0x574936 = null;
+            }
+          }
+        } else {
+          _0x40625c = true;
+        }
+      }
+      function g(b) {
+        function c(b, c, d, e, f) {
+          return a(d - 236, c);
+        }
+        function d(b, c, d, e, f) {
+          return a(f - -354, d);
+        }
+        try {
+          if (c(270, 300, 275, 273, 259) === d(-326, -293, -291, -298, -309)) {
+            _0x492d85 = [];
+          } else {
+            _Czt = false;
+          }
+        } catch (a) {
+          return null;
+        }
+      }
+      if (_jJ == null) {
+        try {
+          console.log(undefined);
+        } catch (a) {}
+      } else {}
+      for (var p = 0; _Px < 5; $xHarLDn++) {
+        try {
+          try {
+            if (u(-515, -510, -510, -502, -501) !== u(-486, -502, -523, -502, -527)) {
+              _0x444a19.log(_0x10cc99);
+            } else {
+              $QGDtsJCD = null;
+            }
+          } catch (a) {}
+        } catch (a) {
+          if (s(321, 322, 324, 333, 345) === t(591, 587, 604, 599, 600)) {
+            try {
+              _0x284ead = [];
+            } catch (a) {}
+          } else {
+            break;
+          }
+        }
+      }
+      try {
+        if (u(-482, -469, -467, -479, -505) !== c(-167, -174, -152, -176, -153)) {
+          var q = [];
+        } else {
+          while ($pbl <= t(640, 614, 629, 590, 625)) {
+            if (d(587, 589, 591, 595, 613) !== c(-165, -181, -164, -172, -174)) {
+              try {
+                if (c(-203, -198, -179, -202, -208) !== c(-191, -198, -172, -213, -215)) {
+                  _0x5a32e4 = false;
+                } else {
+                  _sJRnul = null;
+                }
+              } catch (a) {}
+            } else {
+              try {
+                _0x49a009 = null;
+              } catch (a) {}
+            }
+          }
+        }
+      } catch (a) {}
+      if (typeof $CPswpyrc9 == c(-199, -194, -203, -209, -218)) {
+        try {
+          _OjNo = true;
+        } catch (a) {}
+      } else {}
+      if ($HKOE != c(-146, -160, -141, -174, -155)) {
+        if (c(-150, -165, -144, -146, -184) === s(353, 367, 384, 365, 360)) {
+          try {
+            var r = [];
+          } catch (a) {
+            return null;
+          }
+        } else {
+          try {
+            try {
+              $hpVdUAX = 153;
+            } catch (a) {}
+          } catch (a) {}
+        }
+      } else {}
+    } catch (a) {}
+    function s(b, c, d, e, f) {
+      return a(f - 314, b);
+    }
+    function t(b, c, d, e, f) {
+      return a(c - 582, d);
+    }
+    function u(b, c, d, e, f) {
+      return a(e - -512, c);
+    }
+    return null;
+  })(222);
+  var f;
+  var g = {
+    APP: {
+      NAME: d(-300, -267, -269, -275, -274),
+      VERSION: d(-269, -268, -278, -294, -298),
+      TABS: [d(-273, -279, -238, -258, -279), "音桃".split("").reverse().join(""), c(-174, -166, -162, -175, -154)]
+    },
+    COLORS: {
+      PRIMARY: c(-160, -110, -134, -116, -153),
+      PRIMARY_DARK: j(-558, -539, -557, -544, -535),
+      SECONDARY: d(-303, -262, -290, -283, -257),
+      ACCENT: d(-276, -266, -236, -261, -287),
+      WARNING: c(-157, -135, -132, -126, -131),
+      ERROR: j(-526, -537, -521, -508, -517),
+      SUCCESS: c(-164, -176, -165, -178, -150),
+      INFO: c(-171, -200, -180, -161, -181),
+      BACKGROUND: c(-132, -137, -141, -115, -164),
+      CARD: e(681, 707, 685, 700, 711),
+      TEXT_PRIMARY: h(-470, -469, -474, -465, -467),
+      TEXT_SECONDARY: j(-559, -541, -538, -579, -585),
+      BORDER: h(-446, -435, -424, -459, -441)
+    },
+    ANIMATION: {
+      DURATION: 300,
+      BOUNCE_SCALE: 1.05,
+      PRESS_SCALE: 0.95
+    },
+    TIME_TEXT: {
+      TAOBAO_MESSAGE: j(-540, -517, -527, -534, -530),
+      BEIJING_MESSAGE: j(-534, -547, -547, -548, -520),
+      TENCENT_MESSAGE: j(-548, -561, -554, -533, -561)
+    },
+    PURCHASE_DEFAULTS: {
+      FAST_LOOP_DURATION: 6,
+      SLOW_NORMAL_LOOP_DELAY: 600,
+      SLOW_SHIELD_BREAK_DELAY: 450,
+      BASE_NORMAL_LOOP_DELAY: 500,
+      BASE_SHIELD_BREAK_DELAY: 300,
+      BASE_REFRESH_DELAY: 1388,
+      BASE_FLOW_REFRESH_DELAY: 780,
+      BASE_SAFETY_DELAY: 180,
+      BASE_NORMAL_LOOP_ENTRY_DELAY: 658,
+      BASE_SHIELD_LOOP_ENTRY_DELAY: 258,
+      BOX_CHECK_DELAY: 500,
+      DEFAULT_PURCHASE_TYPE: h(-438, -437, -425, -456, -423),
+      DEFAULT_ORDER_TYPE: d(-271, -282, -265, -260, -260)
+    }
+  };
+  function h(b, c, d, e, f) {
+    return a(b - -489, e);
+  }
+  function i() {
+    var a = ["RDWCJ", "#FF3D00", "流态式刷新", "9xCFSrT", "5175565uTuKJN", "#FF7043", "etSKSx4004751".split("").reverse().join(""), "2.0.2", "CzaHT".split("").reverse().join(""), "WptNM", "JqpyY", "11072136afBXXQ", "kRXEv", "FxFfL", "#6D4C41", "#E55100", "gnirts".split("").reverse().join(""), "2288118cXLnJO", "8559FF#".split("").reverse().join(""), "jQKMt".split("").reverse().join(""), "#FF8F00", "1913408NpTqKv", "3272E3#".split("").reverse().join(""), "件组小纵天".split("").reverse().join(""), "yrurr", "！！！可即间时个这存保就布布拉".split("").reverse().join(""), "天纵Ai 会员系统专业版", "QUFCr", "6EDsdlF", "QatoH".split("").reverse().join(""), "ibevUg641".split("").reverse().join(""), "log", "FFFFFF#".split("").reverse().join(""), "！说多不间时宝淘".split("").reverse().join(""), "bcAFc", "63z5".split("").reverse().join(""), "wSGrQ", "35277jHwhuK", "LSIPXV00081722".split("").reverse().join(""), "北京时间已对齐！", "083BFF#".split("").reverse().join(""), "荐推纵天".split("").reverse().join(""), "kaLFE".split("").reverse().join(""), "拉布布", "3F8FFF#".split("").reverse().join(""), "WlEIa", "#FFCCBC", "#D32F2F", "pJCsC".split("").reverse().join(""), "rkKbX".split("").reverse().join(""), "yp4q".split("").reverse().join(""), "#FF6B35"];
+    i = function () {
+      return a;
+    };
+    return i();
+  }
+  function j(b, c, d, e, f) {
+    return a(b - -570, c);
+  }
+  f = 5;
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = g;
+  }
+  // 确保在 eval 方式加载时提供全局 CONFIG
+  try {
+    if (typeof CONFIG === 'undefined') {
+      CONFIG = g;
+    }
+  } catch (e) {
+    CONFIG = g;
+  }
